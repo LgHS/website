80ebaeda5b554a00b42ae8bc9f6d29fba0e841fc
iooner <contact@iooner.be>
2026-02-17 12:53:07 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,6 @@
 <?php
 $calendar_ics_url = 'https://calendar.google.com/calendar/ical/c_84805eda0a6bebc16e5a5feb5388edc0218c64746c186c158d310d4757bf6994%40group.calendar.google.com/public/basic.ics';
-$spaceapi_url = 'https://corsproxy.io/?https://spaceapi.lghs.be';
+$spaceapi_url = 'https://spaceapi.lghs.be';
+//$spaceapi_url = 'https://corsproxy.io/?https://spaceapi.lghs.be';
 $agenda_days_limit = 42;
 ?>
